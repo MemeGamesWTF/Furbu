5a61e32a559c1ed39d6bd5359d120a2c77e3597a
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-16 18:52:11 +0530

    game

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
     "Build/e2197b9325093a171c5fe777a4ade420.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/801c3c34a2a8489600d3401b34eb1da6.data",
-    "Build/e271753214210dc1cc3844b9e7253f7f.wasm",
+    "Build/647f0e3ef65db7c88e385204f3f0a1d9.data",
+    "Build/29a2911c3c5651e7ce75245590affee2.wasm",
     "TemplateData/style.css"
 
 ];
