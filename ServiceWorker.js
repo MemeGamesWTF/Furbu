4de6526c6c7f75f3e829d685127b37797033c362
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-16 18:58:29 +0530

    audio

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
-    "Build/e2197b9325093a171c5fe777a4ade420.loader.js",
+    "Build/629b751558c2debc4f567fbdf5544ed3.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/647f0e3ef65db7c88e385204f3f0a1d9.data",
-    "Build/29a2911c3c5651e7ce75245590affee2.wasm",
+    "Build/fb35f48f5bfb3047b78989b2610e2576.data",
+    "Build/a89a2c4ff4933ace5a2ebba05d0e299e.wasm",
     "TemplateData/style.css"
 
 ];
