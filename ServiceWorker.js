a048d54fdb3df9cb2fecb3b84f0a661ac53db32c
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-17 17:11:23 +0530

    banner

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
     "Build/c456c6cb7f4ca9917dba4b6f4e0f0505.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/622e18fb3bff30c68ebeda502c3ae512.data",
-    "Build/f3a1fd44c4bfe9f68127db0f14abb240.wasm",
+    "Build/c862c72cd422ce3b0258498458a05376.data",
+    "Build/ce967f26d95e6a77539f7ef069e9c221.wasm",
     "TemplateData/style.css"
 
 ];
