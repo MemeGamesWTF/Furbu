c4b9d0d0fafcd7376772db332053e1755a1d8f98
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-17 22:21:11 +0530

    bug fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
     "Build/bf4ad650adb7c2092964fde1b55bde8b.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/53bdd422e712fbf53b9bf1e3b7385078.data",
-    "Build/0365a1e927d5d3a3a335dfc0fd96d7f1.wasm",
+    "Build/781b98ae2cd5e283163d660c54caf649.data",
+    "Build/b3604aa74ac851d7913cd26c84d7f712.wasm",
     "TemplateData/style.css"
 
 ];
