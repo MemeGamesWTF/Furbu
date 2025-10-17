06638c96ed5b5474c14efd6c248b659b8373744f
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-17 17:17:39 +0530

    ui

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
     "Build/c456c6cb7f4ca9917dba4b6f4e0f0505.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/c862c72cd422ce3b0258498458a05376.data",
+    "Build/ce37539a9728d0198025cc01e4aa0d13.data",
     "Build/ce967f26d95e6a77539f7ef069e9c221.wasm",
     "TemplateData/style.css"
 
