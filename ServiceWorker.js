84304c06c654eb2d1fd8c45843e23c6996ac18cb
Sharanya7705 <sharanyamuraliharan@gmail.com>
2025-10-17 20:21:46 +0530

    feedback

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-JohnnyVerse-1.0";
 const contentToCache = [
-    "Build/d052d1e49e0141b0b7fff29c1015e11b.loader.js",
+    "Build/54bb7a38eee097c20cd73e7a44d2ac4f.loader.js",
     "Build/37e945ff90f1545a6e844458b1fc3945.framework.js",
-    "Build/f6da8d361a222aa984a66254ff1e2967.data",
-    "Build/231b0932f544390ca54e3826f3e21c8c.wasm",
+    "Build/c7eda2ef9c33094c268af1139e6e50a0.data",
+    "Build/e18a8d959d89550be893b98b94d40fc7.wasm",
     "TemplateData/style.css"
 
 ];
